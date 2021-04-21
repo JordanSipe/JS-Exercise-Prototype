8471dc757202056a7b6b87ac51165d9091f27a46
JordanSipe <jsipe2017@gmail.com>
2021-04-21 16:05:02 -0400

    Answered all four MVP questions

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
+  Person.prototype.eat = function(edible){
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
+  Person.prototype.toString = function () {
+    return `${this.name}, ${this.age}`;
+  }
+ const john = new Person('John', 33);
  
 
   
@@ -63,8 +76,15 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  Car.prototype.fill = function(gallons) {
+    this.tank = this.tank + gallons;
   }
   
   
@@ -75,18 +95,24 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
+  }
+
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
   }
  
-  
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. You can call .this in a global scope, but because of strict mode, it will be undefined until put on a value.
+    2. Due to implicit binding, it will point to the object it is called on.
+    3. Due to explicit binding, you can set it to certain value with the .apply(), .call(), .bind() methods.
+    4. You cna use the keyword new to put it on a new variable
   */
   
   
